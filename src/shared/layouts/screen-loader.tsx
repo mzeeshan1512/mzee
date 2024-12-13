554d08c3b8be72ffade03f9cb95be7dafb074128
Muhammad Zeeshan <muhammad.zeeshan@emblemtechnologies.co>
2024-12-13 14:12:18 +0500

    feat and update: update styles for screen loader responsiveness

--- src/shared/layouts/screen-loader.tsx
+++ src/shared/layouts/screen-loader.tsx
@@ -2,15 +2,9 @@ import Image from "next/image";
 import { appIcon, appName } from "../app-config";
 
 const ScreenLoader = () => {
-  return (
-    <div className="w-full h-screen fixed inset-0 flex justify-center items-center">
-      <Image
-        src={appIcon}
-        alt={appName || ""}
-        className={"animate-grayscale aspect-video"}
-      />
-    </div>
-  );
+  return <div className="w-full h-screen fixed inset-0 flex justify-center items-center !bg-neutral-700 bg-opacity-50">
+      <Image src={appIcon} alt={appName || ""} className={"animate-grayscale-transform w-[clamp(200px,30vw,30%)] h-[clamp(100px,5vw,150px)] aspect-video"} />
+    </div>;
 };
 
 export default ScreenLoader;
