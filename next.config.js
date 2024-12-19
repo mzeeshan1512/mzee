db2ff6a477b205c2607867416ffd0f0c72a7c02d
Muhammad Zeeshan <muhammad.zeeshan@emblemtechnologies.co>
2024-12-19 21:05:49 +0500

    fixed  contact-form parsing  issue

--- next.config.js
+++ next.config.js
@@ -6,29 +6,33 @@ const nextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true
   },
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, "styles")]
   },
   images: {
-    domains: ["unsplash.com", "firebasestorage.googleapis.com"],
-    minimumCacheTTL: 60,
+    domains: [
+      "unsplash.com",
+      "firebasestorage.googleapis.com",
+      "lh3.googleusercontent.com"
+    ],
+    minimumCacheTTL: 60
   },
   async redirects() {
     return [
       {
         source: "/",
         destination: "/admin",
-        permanent: true,
+        permanent: true
       },
       {
         source: "/admin",
         destination: "/admin/dashboard",
-        permanent: true,
-      },
+        permanent: true
+      }
     ];
-  },
+  }
 };
 
 module.exports = nextConfig;
--- src/app/(protected)/admin/contact/page.tsx
+++ src/app/(protected)/admin/contact/page.tsx
@@ -12,29 +12,29 @@ const ContactForm = () => {
   const contentList: TableDataKeyList[] = [
     {
       title: "Name",
-      data_key: "name",
+      data_key: "name"
     },
     {
       title: "Email",
-      data_key: "email",
+      data_key: "email"
     },
     {
       title: "Contact Number",
-      data_key: "contact_number",
+      data_key: "contact"
     },
     {
       title: "Subject",
-      data_key: "subject",
+      data_key: "subject"
     },
     {
       title: "Message",
-      data_key: "message",
+      data_key: "message"
     },
     {
       title: "Submitted Date",
       data_key: "created_at",
-      cell: (item: contact_form) => getDateTimeString(item?.created_at),
-    },
+      cell: (item: contact_form) => getDateTimeString(item?.created_at)
+    }
   ];
   return (
     <div className="p-3">
