8256cf2043d81c8649b53535ad067e20531527e4
Muhammad Zeeshan <muhammad.zeeshan@emblemtechnologies.co>
2024-12-20 14:49:09 +0500

    updaed next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -3,21 +3,9 @@ const nextConfig = {
   productionBrowserSourceMaps:
     process?.env?.NODE_ENV === "production" ? false : true,
   reactStrictMode: process?.env?.NODE_ENV === "production" ? false : true,
-  images: {
-    domains: ["unsplash.com", "firebasestorage.googleapis.com"],
-    minimumCacheTTL: 60
-  },
   compiler: {
     removeConsole: process?.env?.NODE_ENV === "production"
   }
-  // webpack: (config, { dev, isServer, productionBrowserSourceMaps }) => {
-  //   console.log({ config });
-  //   // if (!dev && !isServer) {
-  //   //   // Suppress React runtime errors in production
-  //   //   config.optimization.minimize = true;
-  //   // }
-  //   return config;
-  // }
 };
 
 export default nextConfig;
