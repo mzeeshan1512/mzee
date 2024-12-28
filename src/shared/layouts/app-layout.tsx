6edb8ea0b0252965ca577000bde9c78e7441da56
Muhammad Zeeshan <muhammad.zeeshan@emblemtechnologies.co>
2024-12-28 12:11:35 +0500

    remove offline mode handler

--- src/shared/layouts/app-layout.tsx
+++ src/shared/layouts/app-layout.tsx
@@ -5,19 +5,15 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 import dynamic from "next/dynamic";
 import ErrorBoundary from "./error-boundary";
-import { useNetwork } from "../hooks/use-network";
 import { analytics, logEvent } from "../firebase/config";
 import { saveVisit } from "../firebase/use-visit";
 import { ToastContainer } from "../components/toast";
 
-const Offline = dynamic(() => import("./offline-view"), { ssr: true });
 const MaintenanceMode = dynamic(() => import("./maintenance-mode"), {
   ssr: false
 });
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
-  const { online } = useNetwork();
-
   React.useEffect(() => {
     Aos.init();
     Aos.refresh();
@@ -31,14 +27,10 @@ const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ErrorBoundary>
       <ToastContainer />
-      {online ? (
-        process.env.NEXT_PUBLIC_MAINTENANCE_MODE === "true" ? (
-          <MaintenanceMode />
-        ) : (
-          children
-        )
+      {process.env.NEXT_PUBLIC_MAINTENANCE_MODE === "true" ? (
+        <MaintenanceMode />
       ) : (
-        <Offline />
+        children
       )}
     </ErrorBoundary>
   );
